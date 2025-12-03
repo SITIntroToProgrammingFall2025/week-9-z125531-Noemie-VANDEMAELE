8bea19431491a2354832aeb36624dbd2624dfdfa
z125531_Noémie_VANDEMAELE <z125531@shibaura-it.ac.jp>
2025-12-03 17:53:14 +0900

    Add files via upload

--- ahw9-1.c
+++ ahw9-1.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <limits.h>
 
+int main() {
+    int r, g, b;
+    int min_dist_sq = INT_MAX;
+    int current_dist_sq;
+    char *nearest_color = NULL;
+
+    scanf("%d", &r);
+    scanf("%d", &g);
+    scanf("%d", &b);
+
+    current_dist_sq = r * r + g * g + b * b;
+    if (current_dist_sq < min_dist_sq) {
+        min_dist_sq = current_dist_sq;
+        nearest_color = "Black";
+    }
+
+    current_dist_sq = (r - 255) * (r - 255) + g * g + b * b;
+    if (current_dist_sq < min_dist_sq) {
+        min_dist_sq = current_dist_sq;
+        nearest_color = "Red";
+    }
+
+    current_dist_sq = r * r + (g - 255) * (g - 255) + b * b;
+    if (current_dist_sq < min_dist_sq) {
+        min_dist_sq = current_dist_sq;
+        nearest_color = "Green";
+    }
+
+
+    current_dist_sq = r * r + g * g + (b - 255) * (b - 255);
+    if (current_dist_sq < min_dist_sq) {
+        min_dist_sq = current_dist_sq;
+        nearest_color = "Blue";
+    }
+
+
+    current_dist_sq = (r - 255) * (r - 255) + (g - 255) * (g - 255) + (b - 255) * (b - 255);
+    if (current_dist_sq < min_dist_sq) {
+        min_dist_sq = current_dist_sq;
+        nearest_color = "White";
+    }
+
+    printf("The nearest color is %s\n", nearest_color);
+
+    return 0;
+}
--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
 
+int main() {
+    long long arr[5];
+    int n = 5;
+    int i, j, min_idx;
+    long long temp;
+
+    for (i = 0; i < n; i++) {
+        scanf("%lld", &arr[i]);
+    }
+
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+
+    printf("sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%lld%s", arr[i], (i == n - 1) ? "" : " ");
+    }
+    printf("\n");
+
+    return 0;
+}
