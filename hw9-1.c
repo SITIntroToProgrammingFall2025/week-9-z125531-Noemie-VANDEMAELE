c7631f99a5b3bfa3b24d288b110072aefebf4283
z125531_Noémie_VANDEMAELE <z125531@shibaura-it.ac.jp>
2025-12-03 17:45:46 +0900

    Add files via upload

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    float a, b, c, d;
+    float determinant;
+
+    scanf("%f", &a);
+    scanf("%f", &b);
+    scanf("%f", &c);
+    scanf("%f", &d);
+
+    determinant = a * d - b * c;
+
+    printf("You entered\n");
+    printf("%.0f %.0f\n", a, b);
+    printf("%.0f %.0f\n", c, d);
+    printf("Determinant is %.6f\n", determinant);
+
+    return 0;
+}
--- hw9-2.c
+++ hw9-2.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main() {
+    int arr[3][3];
+    int temp;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    printf("You entered\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d%s", arr[i][j], (j == 2) ? "" : " ");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < 3; i++) {
+        temp = arr[i][0];
+        arr[i][0] = arr[i][2];
+        arr[i][2] = temp;
+    }
+
+    printf("\nFlipped array:\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d%s", arr[i][j], (j == 2) ? "" : " ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
