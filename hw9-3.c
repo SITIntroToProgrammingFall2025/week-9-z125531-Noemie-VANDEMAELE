d92964c4b4ed922654a90aa225a13dc5878000bc
z125531_Noémie_VANDEMAELE <z125531@shibaura-it.ac.jp>
2025-12-03 17:56:56 +0900

    Add files via upload

--- hw9-3.c
+++ hw9-3.c
@@ -1,57 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    float A[3][2];
-    float B[2][3];
-    float C[3][3] = {0};
-    int i, j, k;
+    long long arr[5];
+    int n = 5;
+    int i, j, min_idx;
+    long long temp;
 
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 2; j++) {
-            scanf("%f", &A[i][j]);
-        }
+    for (i = 0; i < n; i++) {
+        scanf("%lld", &arr[i]);
     }
 
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 3; j++) {
-            scanf("%f", &B[i][j]);
-        }
-    }
-
-    for (i = 0; i < 3; i++) { 
-        for (j = 0; j < 3; j++) { 
-            for (k = 0; k < 2; k++) { 
-                C[i][j] += A[i][k] * B[k][j];
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
             }
         }
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
     }
 
-    printf("The first matrix you entered is\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 2; j++) {
-            printf("%.1f%s", A[i][j], (j == 1) ? "" : " ");
-        }
-        printf("\n");
+    printf("sorted array:\n");
+    for (i = 0; i < n; i++) {
+        printf("%lld%s", arr[i], (i == n - 1) ? "" : " ");
     }
-
-    printf("The second matrix you entered is\n");
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%.0f%s", B[i][j], (j == 2) ? "" : " ");
-        }
-        printf("\n");
-    }
-    printf("The multiplication product of matrix A and matrix B:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            if (i < 2) {
-                printf("%.1f%s", C[i][j], (j == 2) ? "" : " ");
-            } else {
-                printf("%.0f%s", C[i][j], (j == 2) ? "" : " ");
-            }
-        }
-        printf("\n");
-    }
+    printf("\n");
 
     return 0;
 }
