78d37c65f04a39f3b5de3b6c406f13d260c670d8
z125531_Noémie_VANDEMAELE <z125531@shibaura-it.ac.jp>
2025-12-03 18:01:36 +0900

    Add files via upload

--- hw9-2.c
+++ hw9-2.c
@@ -1,63 +1,35 @@
 #include <stdio.h>
 
 int main() {
-    float A[3][2];
-    float B[2][3];
-    float C[3][3] = {0};
-    int i, j, k;
+    int arr[3][3];
+    int temp;
 
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 2; j++) {
-            scanf("%f", &A[i][j]);
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%d", &arr[i][j]);
         }
     }
 
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 3; j++) {
-            scanf("%f", &B[i][j]);
+    printf("You entered\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);
         }
+        printf("\n");
     }
 
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            for (k = 0; k < 2; k++) {
-                C[i][j] += A[i][k] * B[k][j];
-            }
-        }
+    for (int i = 0; i < 3; i++) {
+        temp = arr[i][0];
+        arr[i][0] = arr[i][2];
+        arr[i][2] = temp;
     }
 
-    printf("The first matrix you entered is\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 2; j++) {
-            printf("%.1f%s", A[i][j], (j == 1) ? "" : " ");
+    printf("Output\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%d ", arr[i][j]);
         }
-        if (i < 2) {
-            printf(" \n");
-        } else {
-            printf("\n");
-        }
-    }
-    printf("\n");
-
-    printf("The second matrix you entered is\n");
-    for (i = 0; i < 2; i++) {
-        for (j = 0; j < 3; j++) {
-            printf("%.0f%s", B[i][j], (j == 2) ? "" : " ");
-        }
-        printf(" \n");
-    }
-    printf("\n");
-
-    printf("The multiplication product of matrix A and matrix B:\n");
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            if (i == 0) {
-                printf("%.1f%s", C[i][j], (j == 2) ? "" : " ");
-            } else {
-                printf("%.0f%s", C[i][j], (j == 2) ? "" : " ");
-            }
-        }
-        printf(" \n");
+        printf("\n");
     }
 
     return 0;
