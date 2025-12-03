b6ea8fc55eb8c36cbab711d6c10db6084d9e3131
z125531_Noémie_VANDEMAELE <z125531@shibaura-it.ac.jp>
2025-12-03 18:03:45 +0900

    Add files via upload

--- hw9-3.c
+++ hw9-3.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
 
 int main() {
-    long long arr[5];
-    int n = 5;
-    int i, j, min_idx;
-    long long temp;
 
-    for (i = 0; i < n; i++) {
-        scanf("%lld", &arr[i]);
-    }
+    double A[2][3];
+    double B[3][2];
+    double C[2][2];
 
-    for (i = 0; i < n - 1; i++) {
-        min_idx = i;
-        for (j = i + 1; j < n; j++) {
-            if (arr[j] < arr[min_idx]) {
-                min_idx = j;
-            }
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            scanf("%lf", &A[i][j]);
         }
-        temp = arr[min_idx];
-        arr[min_idx] = arr[i];
-        arr[i] = temp;
     }
 
-    printf("sorted array:\n");
-    for (i = 0; i < n; i++) {
-        printf("%lld%s", arr[i], (i == n - 1) ? "" : " ");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            scanf("%lf", &B[i][j]);
+        }
     }
-    printf("\n");
+
+    printf("The first matrix you entered is\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("%g%s", A[i][j], (j == 2) ? " " : " ");
+        }
+        printf("\n");
+    }
+
+    printf("The second matrix you entered is\n");
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%g%s", B[i][j], (j == 1) ? " " : " ");
+        }
+        printf("\n");
+    }
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            C[i][j] = A[i][0] * B[0][j] +
+                      A[i][1] * B[1][j] +
+                      A[i][2] * B[2][j];
+        }
+    }
+
+    printf("The multiplication product of matrix A and matrix B:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%g%s", C[i][j], (j == 1) ? "" : " ");
+        }
+        printf("\n");
+    }
 
     return 0;
 }
